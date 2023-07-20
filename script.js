5a9cf6dc49e5257d2b97a25a93bba7e26a0591f7
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2023-07-20 16:03:53 +0800

    Add a getChoiceValue function to return an integer representation of a choice

--- script.js
+++ script.js
@@ -15,4 +15,28 @@ function getComputerChoice() {
     return choice;
 }
 
+function play(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    let playerChoiceValue, computerChoiceValue;
+
+    //if()
+    if(playerSelection === computerSelection) {
+        return `Tie! Both of you chose ${playerSelection}`;
+    }
+    else if(playerSelection === "rock" && computer)
+}
+
+function getChoiceValue(str) {
+    str = str.toLowerCase();
+    if(str === "rock") {
+        return -1;
+    }
+    else if(str === "paper") {
+        return 0;
+    }
+    else if(str === "scissors") {
+        return 1;
+    }
+}
 console.log(getComputerChoice());
