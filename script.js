0b7f4277f8b5681af4041bd84f8d5b565cf04ff5
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2023-07-27 17:51:27 +0800

    Add new algorithm with event listener

--- script.js
+++ script.js
@@ -1,73 +1,42 @@
 const choices = Array.from(document.getElementsByClassName('choice'));
+const rock_paper_or_scissors = [-1, 0, 1];
+// global so that it can be accessed and reverted back to 0
+let playerScore = 0;
+let computerScore = 0;
 
-function play(){
-    choices.forEach(choice => {
-        choice.addEventListener('click', getPlayerChoice);
-    });
+function getRpsValue(buttonId) {
+    if(buttonId === 'rock') return -1;
+    else if(buttonId === 'paper') return 0;
+    else if(buttonId === 'scissors') return 1;
 }
 
 function getComputerChoice() {
-    randNum = Math.floor(Math.random() * 100);
-    randNum = Math.ceil(randNum / 33);
-    choice = "none";
-    if(randNum === 1) {
-        choice = "Rock";
-    }
-    else if(randNum === 2) {
-        choice = "Paper";
-    }
-    else if(randNum === 3) {
-        choice = "Scissors";
-    }
-    // console.log(randNum);
-    return choice;
+    return rock_paper_or_scissors[Math.floor(Math.random() * 3)];
 }
 
-function play2(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+function playRound(event) {
+    const playerChoice = getRpsValue(event.target.id);
+    const computerChoice = getComputerChoice();
 
-    // gets integer representation of the selection
-    let playerChoiceValue = getChoiceValue(playerSelection);
-    let computerChoiceValue = getChoiceValue(computerSelection);
+    const displayedPlayerScore = document.getElementById('human-point');
+    const displayedComputerScore = document.getElementById('computer-point');
 
-    result = playerChoiceValue - computerChoiceValue;
+    const result = playerChoice - computerChoice;
+
+    if(result === 1 || result === -2) playerScore++;
+    else if(result === -1 || result === 2) computerScore++;
     
-    if(result === 0) {
-        return `Tie! Both of you chose ${playerSelection}`;
-    }
-    else if(result === 1 || result === -2) {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else if(result === -1 || result === 2) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
-    }
-    else {
-        return "Invalid choice!";
+    displayedPlayerScore.textContent = playerScore;
+    displayedComputerScore.textContent = computerScore;
+
+    if(playerScore === 5 || computerScore === 5) {
+        playerScore = 0;
+        computerScore = 0;
+        displayedPlayerScore.textContent = playerScore;
+        displayedComputerScore.textContent = computerScore;
     }
 }
 
-function getChoiceValue(str) {
-    str = str.toLowerCase();
-    if(str === "Rock") {
-        return -1;
-    }
-    else if(str === "Paper") {
-        return 0;
-    }
-    else if(str === "Scissors") {
-        return 1;
-    }
-    else {
-        return -10;
-    }
-}
-
-function getPlayerChoice(e) {
-    const choice = e.target.id;
-    console.log(choice);
-}
-play();
-// document.getElementById('rock').addEventListener('click', getPlayerChoice);
-// document.getElementById('paper').addEventListener('click', getPlayerChoice);
-// document.getElementById('scissors').addEventListener('click', getPlayerChoice);
+choices.forEach(choice => {
+    choice.addEventListener('click', playRound);
+});
